e86572f47f27dfdc45598f95f7cb87a9eeec60bd
extrich <extridev@gmail.com>
2017-10-24 01:13:27 +0900

    DB연결 테스트 성공

--- dbconnector.py
+++ dbconnector.py
@@ -1,13 +1,16 @@
 import pymysql
 import sshtunnel
 
-class db_connector:
+class DBConnector:
     def __init__(self):
         self.server = None
         self.conn = None
         self.cursor = None
     def db_connect(self, db_host, db_port, user_id, password, db_name):
-        self.conn = pymysql.connect(host=db_host, port=int(db_port), user=user_id, passwd=password, db=db_name)
+        if self.server is None:
+            self.conn = pymysql.connect(host=db_host, port=int(db_port), user=user_id, passwd=password, db=db_name)
+        else:
+            self.conn = pymysql.connect(host=db_host, port=self.server.local_bind_port, user=user_id, passwd=password, db=db_name)
         self.cursor = self.conn.cursor()
     def ssh_connect(self, ssh_host, ssh_port, ssh_id, ssh_passwd, remote_bind_addr, db_port):
         self.server=sshtunnel.SSHTunnelForwarder((ssh_host, int(ssh_port)), ssh_password=ssh_passwd, ssh_username=ssh_id, remote_bind_address=(remote_bind_addr, int(db_port)))
